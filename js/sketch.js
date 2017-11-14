be25674a49e145b6a0007244810142c3298f42ad
miatangvik <mita.94@hotmail.com>
2017-11-14 19:11:03 +0100

    Delete accidentally added files

--- js/sketch.js
+++ js/sketch.js
@@ -9,20 +9,17 @@ var items;
 var platformHeight = 1;
 var platformFullHeight = 1000;
 
-
 var backgroundImg;
 
 var playerIdle, playerForwards, playerBackwards, playerJumping;
 
-
-// Gravity & jumps
+// Gravity and jumps
 const GRAVITY = 0.3;
 var jumping = false;
 
 // Score
 var score = 0;
 var scoreCount = document.querySelector('#score');
-
 
 // Items size
 var itemWidth = 15;
@@ -32,7 +29,7 @@ platformsArray.push({
     x: 200,
     y: 400,
     width: 600,
-    height: platformHeight      
+    height: platformHeight
 });
 
 platformsArray.push({
@@ -81,13 +78,13 @@ function preload() {}
 
 function setup() {
     createCanvas(800, 480);
-    
+
     backgroundImg = loadImage("assets/background.jpg");
-    
+
     platforms = new Group();
     fallingObjects = new Group();
     items = new Group();
-    
+
     // Create platforms
     for (var i = 0; i < platformsArray.length; i++) {
         var newPlatform = createSprite(platformsArray[i].x, platformsArray[i].y, platformsArray[i].width, platformsArray[i].height);
@@ -96,24 +93,24 @@ function setup() {
         newPlatform.shapeColor = color(25, 25, 25);
         platforms.add(newPlatform);
     }
-        
+
     // Create falling objects
     for (var i = 0; i < fallingObjectsArray.length; i++) {
         var newFallingObject = createSprite(fallingObjectsArray[i].x, fallingObjectsArray[i].y, fallingObjectsArray[i].width, fallingObjectsArray[i].height);
         newFallingObject.shapeColor = color(0);
         fallingObjects.add(newFallingObject);
     }
-    
+
     // Create items
     for (var i = 0; i < itemsArray.length; i++) {
         var newItem = createSprite(itemsArray[i].x, itemsArray[i].y, itemsArray[i].width, itemsArray[i].height);
         newItem.shapeColor = color(255, 255, 0);
         items.add(newItem);
     }
-        
+
     // Create player
     player = createSprite(100, 50, 15, 25);
-    
+
     playerIdle = loadImage("assets/dude-idle.png");
     playerForwards = loadImage("assets/dude-forwards.png");
     playerBackwards = loadImage("assets/dude-backwards.png");
@@ -122,41 +119,41 @@ function setup() {
     player.addImage(playerForwards);
 }
 
-function draw() {    
+function draw() {
     background("#191919");
-    image(backgroundImg, 0 - width/2, 0 -height/2);
-    
+    image(backgroundImg, 0 - width / 2, 0 - height / 2);
+
     platforms[0].debug = true;
-    
+
     // Add gravity
     player.velocity.y += GRAVITY;
-    
+
     // Set camera position
     camera.position.x = player.position.x;
     camera.position.y = player.position.y;
-    
+
     // Stop 
     for (var i = 0; i < platforms.length; i++) {
-        player.collide(platforms[i], function() {
+        player.collide(platforms[i], function () {
             player.position.y = player.position.y;
             player.velocity.y = GRAVITY;
         });
     }
-    
+
     // Remove items on overlap
     player.overlap(items, getItem);
-    
+
     // Move player with keys
     if (keyIsDown(RIGHT_ARROW)) {
         player.position.x += 4;
         player.addImage(playerForwards);
     }
-    
+
     if (keyIsDown(LEFT_ARROW)) {
         player.position.x -= 4;
         player.addImage(playerBackwards);
     }
-    
+
     if (keyWentDown(' ')) {
         console.log(player.velocity.y);
         if (!jumping && player.velocity.y == GRAVITY) {
@@ -165,7 +162,7 @@ function draw() {
         }
         jumping = false;
     }
-    
+
     drawSprites();
 }
 
