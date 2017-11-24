2c802ad643bf5b28bd06fcf7ba51aa359f25bd76
Mia Tangvik <miatangvik@users.noreply.github.com>
2017-11-24 13:33:35 +0100

    Fix level structure and overlap on player's sides

--- js/sketch-kopi.js
+++ js/sketch-kopi.js
@@ -1,89 +1,188 @@
 var player;
-var platforms;
+var gravity = 1;
+var jumping = false;
+
+// Groups
+var platforms, items, interactables, climbables;
 
 // Render Scene
-var sceneWidth  = 3600;
+var sceneWidth = 3400;
 var sceneHeight = 1700;
 
 // Images
 //var levelBackground, levelForeground;
 var playerIdle, playerForwards, playerBackwards, playerJumping;
 
-// Gravity and jumps
-var gravity = 1;
-var jumping = false;
+// Score
+var score = 0;
+var scoreCount = document.querySelector('#score');
 
-
-
-function preload() {
-}
-
-
+function preload() {}
 
 function setup() {
     createCanvas(1000, 500);
-    
+
     // Create groups
     platforms = new Group();
-    
+    items = new Group();
+    interactables = new Group();
+    climbables = new Group();
+
     // Add platforms
-    var platform1 = createSprite(width/2, height/2);
+    var platform1 = createSprite(250 + 250, 400);
     platform1.addImage(loadImage("assets/level0.png"));
     platforms.add(platform1);
+
+    var platform2 = createSprite(250 + 1210, 490);
+    platform2.addImage(loadImage("assets/level3.png"));
+    platforms.add(platform2);
+
+    var platform3 = createSprite(250 + -270, 748);
+    platform3.addImage(loadImage("assets/level_bottom_1.png"));
+    platforms.add(platform3);
+
+    var platform4 = createSprite(250 + 355, 912);
+    platform4.addImage(loadImage("assets/level_bottom_2.png"));
+    platforms.add(platform4);
+
+    var platform5 = createSprite(250 + 974, 912);
+    platform5.addImage(loadImage("assets/level_bottom_3.png"));
+    platforms.add(platform5);
+
+    var platform6 = createSprite(250 + 1599, 663.5);
+    platform6.addImage(loadImage("assets/level_bottom_4.png"));
+    platforms.add(platform6);
+
+    // Add items
+    var item1 = createSprite(120, 650, 10, 10);
+    item1.shapeColor = color(255, 255, 0);
+    items.add(item1);
     
+    // Add interactables
+    var interactable1 = createSprite(540, 250, 30, 30);
+    interactable1.shapeColor = color(0, 144, 67);
+    interactables.add(interactable1);
+    
+    // Add climbables
+    var climbable1 = createSprite(1710, 490, 20, 400);
+    climbable1.shapeColor = color(82, 38, 0);
+    climbables.add(climbable1);
+
     // Create player
     player = createSprite(100, 0);
-    playerForwards  = loadImage("assets/dude-forwards.png");
+    playerForwards = loadImage("assets/dude-forwards.png");
     playerBackwards = loadImage("assets/dude-backwards.png");
     player.addAnimation("normal", "assets/dude-backwards.png");
 }
 
 function draw() {
     background("#2a2e33");
-    
-    player.velocity.y += gravity;
-    
+
     // Set camera position
     camera.position.x = player.position.x + 150;
     camera.position.y = player.position.y - 50;
-    
-    player.overlap(platforms, function(player, platform){
-        if (platform.overlapPixel(player.position.x, player.position.y + (player.height / 2))) {
+    camera.zoom = 1;
+
+    player.overlap(platforms, function (player, platform) {
+
+        // Check if the player is standing on a platform
+        if (platform.overlapPixel(player.position.x, player.position.y + player.height / 2)) {
             player.velocity.y = 0;
             player.position.y -= 1;
             jumping = false;
-            
-            while (platform.overlapPixel(player.position.x, player.position.y + (player.height / 2))) {
-                player.position.y -= 3;
+
+            while (platform.overlapPixel(player.position.x, player.position.y + player.height / 2)) {
+                player.position.y -= 1;
+            }
+        }
+
+        // Check if the player's head is hitting a platform
+        if (platform.overlapPixel(player.position.x, player.position.y - player.height / 2)) {
+            player.position.y += 1;
+
+            while (platform.overlapPixel(player.position.x, player.position.y - player.height / 2)) {
+                player.position.y += 1;
+            }
+        }
+
+        // Check if the player's left side is hitting a platform
+        if (platform.overlapPixel(player.position.x - (player.width / 2) - 10, player.position.y - player.height / 2)) {
+            player.position.x += 1;
+
+            while (platform.overlapPixel(player.position.x - (player.width / 2) - 10, player.position.y - player.height / 2)) {
+                player.position.x += 1;
+            }
+        }
+
+        // Check if the player's right side is hitting a platform
+        if (platform.overlapPixel(player.position.x + (player.width / 2) + 10, player.position.y - player.height / 2)) {
+            player.position.x -= 1;
+
+            while (platform.overlapPixel(player.position.x + (player.width / 2) + 10, player.position.y - player.height / 2)) {
+                player.position.x -= 1;
             }
         }
     });
 
+    player.velocity.y += gravity;
+
+    // Remove item on overlap
+    player.overlap(items, getItem);
+    
+    // Interact when player overlaps interactable and presses x
+    player.overlap(interactables, interact);
+    
+    // Climb up when player overlaps climbable area arrow up is pressed, climb down when arrow down is pressed
+    player.overlap(climbables, climb);
+
     // Limit player movements
-    if (player.position.x < 0 + player.width / 2) player.position.x = 0 + player.width / 2;
+    if (player.position.x < player.width / 2) player.position.x = player.width / 2;
     /*if (player.position.x > sceneWidth) player.position.x = sceneWidth;
     if (player.position.y > sceneHeight - player.width / 2) player.position.y = sceneHeight - player.width / 2;*/
-    
+
     // Jump
     if (keyWentDown(' ')) {
-        
         if (!jumping) {
             jumping = true;
-            player.position.y -= 5;
             player.velocity.y -= 15;
         }
     }
-    
-    // Move player with keys
-    if (keyIsDown(RIGHT_ARROW)){
-        player.position.x += 4;
+
+    // Move player with arrow keys
+    if (keyIsDown(RIGHT_ARROW)) {
+        player.position.x += 8;
         player.addImage(playerForwards);
     }
 
-    if (keyIsDown(LEFT_ARROW)){
-        player.position.x -= 4;
+    if (keyIsDown(LEFT_ARROW)) {
+        player.position.x -= 8;
         player.addImage(playerBackwards);
     }
 
     drawSprites();
+}
+
+function getItem(player, item) {
+    item.remove();
+    score += 1;
+    scoreCount.innerHTML = score;
+}
+
+function interact(player, interactable) {
+    if (keyWentDown(88)) {
+        interactable.remove();
+    }
+}
+
+function climb(player, climbable) {
+    player.velocity.y = 0;
+    player.position.y = player.position.y;
+
+    if (keyIsDown(UP_ARROW)) {
+        player.position.y -= 4;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+        player.position.y += 4;
+    }
 }
