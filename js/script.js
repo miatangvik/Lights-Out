6ff61b10dac2d6591f67d2f6ee7b8aa4005b6253
miatangvik <mita.94@hotmail.com>
2017-12-01 09:50:09 +0100

    Work on bag

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var gameOverlay = document.querySelector('#game-overlay');
 var itemBagButton = document.querySelector('#item-bag');
 
+/*var clues = document.querySelectorAll(".clue");
+var clueContent = document.querySelectorAll(".clue-content");*/
+
+var clues = document.getElementsByClassName("clue");
+var clueContent = document.getElementsByClassName("clue-content");
+
 itemBagButton.addEventListener('mousedown', function () {
     gameOverlay.classList.toggle('active');
 });
@@ -10,3 +16,11 @@ document.addEventListener('keydown', function (event) {
         gameOverlay.classList.toggle('active');
     }
 });
+
+for (var i = 0; i < clues.length; i++) {
+    clues[i].addEventListener('mousedown', function (i) {
+        console.log("hei");
+        console.log(clueContent.length);
+        clueContent[i].style.background = "black";
+    }, false);
+}
